b1e382b81cce3b02886e6d7d68f26c5e73fbcf0b
Jesse Cooper <jesse.cooper@codeholics.com>
2021-10-22 14:11:17 +0100

    update the sum and difference calculations

--- __init__.py
+++ __init__.py
@@ -15,10 +15,43 @@ class Calculator(MycroftSkill):
 
     @intent_file_handler('sum.intent')
     def handle_sum(self, message):
+        # TODO issue with decimal points
+        self.log.info("lang: %s", self.lang)
+        self.log.info("raw numbers var: %s", message.data.get('numbers'))
         numbers = extract_numbers(message.data.get('numbers'))
-        self.log.info("maessage: %s", numbers)
+        self.log.info("numbers: %s", numbers)
         answer = sum(numbers)
-        self.speak_dialog('calculator', {'answer': answer})
+        # answer as an int if it is not a float
+        self.speak_dialog('calculator', {'answer': int(answer) if answer.is_integer() else answer})
+
+    @intent_file_handler('difference.intent')
+    def handle_difference(self, message):
+        self.log.info("raw numbers var: %s", message.data.get('numbers'))
+        numbers = extract_numbers(message.data.get('numbers'))
+        self.log.info("numbers: %s", numbers)
+        if len(numbers) > 2:
+            self.speak_dialog('not_implemented')
+        else:
+            if message.data.get('numbers').find('from') != -1:
+                answer = numbers[1]-numbers[0]
+            else:
+                answer = numbers[0]-numbers[1]
+            # answer as an int if it is not a float
+            self.speak_dialog('calculator', {'answer': int(answer) if answer.is_integer() else answer})
+
+    @intent_file_handler('percentage.intent')
+    def handle_percentage(self, message):
+        self.log.info("raw numbers var: %s", message.data.get('numbers'))
+        numbers = extract_numbers(message.data.get('numbers'))
+        self.log.info("numbers: %s", numbers)
+        if len(numbers) > 2:
+            self.speak_dialog('not_implemented')
+        else:
+            if numbers[0] != 0:
+                answer = (numbers[1]-numbers[0])/numbers[0]*100
+            else:
+                self.speak_dialog('divide_by_zero')
+            self.speak_dialog('calculator', {'answer': f"{int(answer) if answer.is_integer() else answer} percent"})
 
 
 def create_skill():
